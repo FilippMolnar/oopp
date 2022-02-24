9c312eea88b33ffd802ae158c15bdda5b64a73a7
Izzy van der Giessen <i.vandergiessen@student.tudelft.nl>
2022-02-24 16:28:23 +0100

    Add api test

---
+++ server/src/test/java/server/api/WaitControllerTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package server.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import commons.Person;
+import commons.Quote;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class WaitControllerTest {
+
+    private WaitController sut;
+    private List<String> lobby;
+
+    @BeforeEach
+    public void setup() {
+        sut = new WaitController();
+        lobby = new ArrayList();
+    }
+
+    @Test
+    public void addsNameTest() {
+        var actual = sut.addName("Name");
+        lobby.add("Name");
+        assertEquals(lobby, actual);
+    }
+
+}

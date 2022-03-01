479174370a4d4a3e799912dc800fcf101bf11b05
Lieke Sanders <L.Sanders@student.tudelft.nl>
2022-03-01 15:05:27 +0100

    Add error messages for players entering their name

--- client/src/main/java/client/controllers/EnterNameCtrl.java
+++ client/src/main/java/client/controllers/EnterNameCtrl.java
@@ -1,10 +1,13 @@
 package client.controllers;
 
 import client.utils.ServerUtils;
+import commons.Player;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class EnterNameCtrl {
 
@@ -14,18 +17,42 @@ public class EnterNameCtrl {
     @FXML
     private TextField nameString;
 
+    @FXML
+    private Label labelErrors;
+
     @Inject
     EnterNameCtrl(ServerUtils serverUtils,MainAppController appController){
         this.appController = appController;
         this.serverUtils = serverUtils;
-
     }
 
     public void enterRoom(){
+        labelErrors.setText("");
         String name = nameString.getText();
-        this.appController.enterWaitingRoom(name);
-        this.serverUtils.postName(name);
-        // this.serverUtils.send("/app/waitingRoom", new Player(name));
-        // this line of code was used to send the data through the socket connection
+        String finalName = name.substring(0,15);
+        if (name != finalName) {
+            // Send message to player that their name was too long
+            labelErrors.setText("Your name was too long, we limited the number of characters");
+        }
+        // Get request for the players that are currently waiting
+        List<Player> playersInWaitingRoom = serverUtils.getAllNamesInWaitingRoom();
+        if (!playersInWaitingRoom.isEmpty()) {
+            // Check if the player has a unique name
+            boolean flag = true;
+            for (int i = 0; i < playersInWaitingRoom.size(); i++) {
+                if (playersInWaitingRoom.get(i).getName().equals(finalName)) { // Another player with this name was found
+                    labelErrors.setText("This name is already taken, please try another");
+                    flag = false;
+                }
+            }
+            if (flag) {
+                this.appController.enterWaitingRoom(finalName);
+                this.serverUtils.postName(finalName);
+            }
+        }
+        else { // If the waiting room is empty, the player will for sure have a unique name
+            this.appController.enterWaitingRoom(finalName);
+            this.serverUtils.postName(finalName);
+        }
     }
 }

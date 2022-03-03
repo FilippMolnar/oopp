b6d9fde7ebb2ed19a7fd71bbeaf2bd5d55b9670c
FilipMolnar <F.M.T.Molnar@student.tudelft.nl>
2022-03-03 11:41:29 +0100

    added questions scenes and controllers

---
+++ client/src/main/java/client/controllers/QuestionMultiOptionsCtrl.java
@@ -0,0 +1,50 @@
+package client.controllers;
+
+import client.scenes.template.MainCtrl;
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import javafx.scene.input.KeyEvent;
+
+public class QuestionMultiOptionsCtrl {
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @Inject
+    public QuestionMultiOptionsCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+
+    }
+
+    public void pressedA() {
+        mainCtrl.showOverview();
+    }
+    public void pressedB() {
+        mainCtrl.showOverview();
+    }
+    public void pressedC() {
+        mainCtrl.showOverview();
+    }
+
+    private void clearFields() {
+        return;
+    }
+
+    public void cancel() {
+        clearFields();
+        mainCtrl.showOverview();
+    }
+
+    public void keyPressed(KeyEvent e) {
+        switch (e.getCode()) {
+            case ENTER:
+                pressedA();
+                break;
+            case ESCAPE:
+                cancel();
+                break;
+            default:
+                break;
+        }
+    }
+}

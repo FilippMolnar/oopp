2aa98ded702959aead93c3c5ce210b5bfbb3ae8f
Mihai Maria Teodora <m.t.mihai-1@student.tudelft.nl>
2022-03-11 13:00:13 +0100

    Solve bug created by deleting unused imports and then solving merge conflicts

--- client/src/main/java/client/controllers/QuestionMultiOptionsCtrl.java
+++ client/src/main/java/client/controllers/QuestionMultiOptionsCtrl.java
@@ -2,7 +2,11 @@ package client.controllers;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.Question;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -10,13 +14,17 @@ import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ResourceBundle;
 
 
-public class QuestionMultiOptionsCtrl  {
+public class QuestionMultiOptionsCtrl  implements Initializable {
     private final ServerUtils server;
     private final MainAppController mainCtrl;
     private Question question;

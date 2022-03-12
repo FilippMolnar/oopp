06d45dc4b534b50353ddfa616b4b46865dd712bc
Ivan Smilenov <I.Y.Smilenov@student.tudelft.nl>
2022-03-12 23:03:07 +0100

    Fetching Leaderboard gives a sorted list of scores with names

--- commons/src/main/java/commons/Game.java
+++ commons/src/main/java/commons/Game.java
@@ -1,9 +1,9 @@
 package commons;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.*;
 
 /**
  *  TO BE TESTED
@@ -13,16 +13,24 @@ public class Game {
     private int size;
     private List<Question> questions = new ArrayList<>();
     private List<Player> players = new ArrayList<>();
-    private Map<Player, Integer> scoreboard = new HashMap<>(); // Maps a player to his current score
+    private List<Integer> scoreboard = new ArrayList<>(); // A list of the scores according to playerID
+    private Map<Player, Integer> playerToID = new HashMap<>(); // Maps a player to his id
+    private Map<Integer, Player> idToPlayer = new HashMap<>(); // Maps a playerID to player
+    private Map<Integer , Boolean> inGame = new HashMap<>(); // Keeps track if player with id is in game
     private int count = 0; // Keeps track of how many players have requested a new question
     private int qnum = 0 ; // Keeps track on which question we are throughout the game
+    private int pnum = 0 ; // Keeps track of what the next player`s id should be
+    private int pInGame = 0 ; // Keeps track of how many players are in current game
 
     public Game(){}
 
     public Game(int gameID, List<Player> players) {
         this.gameID = gameID;
-        this.players = players;
         this.size = players.size();
+        for(var player:players)
+        {
+            addPlayer(player);
+        }
     }
 
     public int getSize() {
@@ -78,12 +86,24 @@ public class Game {
 
     public void addPlayer(Player player)
     {
-        this.players.add(player);
+        inGame.put(pnum,true);
+        pInGame++;
+        players.add(player);
+        scoreboard.add(0);
+        playerToID.put(player,pnum);
+        idToPlayer.put(pnum,player);
+        pnum++;
     }
 
+    /**
+     * TODO optimize if necessary
+     * Currently is set to only make the player 'disabled'
+     * @param player to be removed
+     */
     public void removePlayer(Player player)
     {
-        this.players.remove(player);
+        inGame.replace(playerToID.get(player),false);
+        pInGame--;
     }
 
     public List<Player> getPlayers() {
@@ -94,17 +114,39 @@ public class Game {
         this.players = players;
     }
 
-    public Map<Player, Integer> getScoreboard() {
-        return scoreboard;
+    /**
+     * Retrieves a sorted scoreboard
+     * @return a list of pairs of score and player in descending order
+     */
+
+    public List<Pair<Integer,Player>> getLeaderboard() {
+
+        List<Pair<Integer,Player>> scores = new ArrayList<>();
+        for(int i=0;i<pnum;i++)
+        {
+            if(inGame.get(i) == false)continue; // Players which left the game won`t be in the scoreboard
+            Pair<Integer,Player> cscore = new ImmutablePair<Integer,Player>(scoreboard.get(i),idToPlayer.get(i));
+            scores.add(cscore);
+        }
+        Collections.sort(scores, new Comparator<Pair<Integer,Player>>(){
+            @Override
+            public int compare(final Pair<Integer,Player> p1 , final Pair<Integer,Player> p2)
+            {
+                if(p1.getKey()<p2.getKey())return 1;
+                else return -1;
+            }
+        });
+        return scores;
     }
 
-    public void setScoreboard(Map<Player, Integer> scoreboard) {
+    public void setScoreboard(List<Integer> scoreboard) {
         this.scoreboard = scoreboard;
     }
 
     public void setScore(Player player,int score)
     {
-        this.scoreboard.replace(player,score);
+        int id = playerToID.get(player);
+        scoreboard.set(id,score);
     }
 
     /**
--- server/src/main/java/server/api/GameController.java
+++ server/src/main/java/server/api/GameController.java
@@ -2,13 +2,16 @@ package server.api;
 
 import commons.Game;
 import commons.Player;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RestController
 public class GameController {
 
     private static Map<Integer, Game> games = new HashMap<>();
@@ -25,6 +28,11 @@ public class GameController {
         games.get(gameID).addPlayer(player);
     }
 
+    public static void setScore(int gameID,Player player, int score)
+    {
+        games.get(gameID).setScore(player,score);
+    }
+
     public static void removePlayer(int gameID,Player player)
     {
         games.get(gameID).removePlayer(player);
@@ -38,13 +46,13 @@ public class GameController {
     /**
      * Retrieve the leaderboard for the current game
      * @param gameID identifier for the current game
-     * @return a map which contains every player and his score for this game
+     * @return a list of pairs of score and player sorted in descending order by their score
      */
     @GetMapping(path = "api/leaderboard/{gameID}")
-    public Map<Player,Integer> getScoreboard(@PathVariable("gameID") int gameID)
+    public static List<Pair<Integer,Player>> getLeaderboard(@PathVariable("gameID") int gameID)
     {
         Game cur = getGame(gameID);
 
-        return cur.getScoreboard();
+        return cur.getLeaderboard();
     }
 }

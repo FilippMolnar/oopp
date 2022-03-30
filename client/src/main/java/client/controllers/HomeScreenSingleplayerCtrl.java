afd8f43ca6ce42657cbf270e04d93362c1617265
FilipMolnar <F.M.T.Molnar@student.tudelft.nl>
2022-03-30 16:52:29 +0200

    init server for SP

--- client/src/main/java/client/controllers/HomeScreenSingleplayerCtrl.java
+++ client/src/main/java/client/controllers/HomeScreenSingleplayerCtrl.java
@@ -35,6 +35,7 @@ public class HomeScreenSingleplayerCtrl {
             labelErrors.setText("Your name was too long, we limited the number of characters");
         }
 
+        serverUtils.initializeServer("localhost");
         ArrayList<Question> questions = serverUtils.getRandomQuestions();
         appController.addQuestionScenes(questions, 1);
         this.appController.setName(finalName);

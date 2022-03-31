eae074bc4447fab3750acfc93ef69b6c6d9afacc
FilipMolnar <F.M.T.Molnar@student.tudelft.nl>
2022-04-01 00:24:18 +0200

    answer test

--- commons/src/test/java/commons/AnswerTest.java
+++ commons/src/test/java/commons/AnswerTest.java
@@ -20,13 +20,67 @@ class AnswerTest {
         assertEquals(ans.getOption(),"option2");
     }
 
-    /*@Test
+    @Test
     void testToString() {
-        assertEquals(answer.toString(),"Answer{isCorrect=false, option='option'}");
-    }*/
+        String s = "Answer{isCorrect=false, option='option', score=100, username=Ana}";
+        assertEquals(answer.toString(),s);
+    }
+
 
     @Test
     void testEquals() {
         assertTrue(answer.equals(new Answer(false,"option",1,100,"Ana")));
+        assertFalse(answer.equals(new Answer(false,null,1,100,null)));
+        assertFalse(answer.equals(ans));
+    }
+
+    @Test
+    void testGetGameId() {
+        assertEquals(answer.getGameID(),1);
+        assertNotEquals(ans.getGameID(),2);
+    }
+
+    @Test
+    void testSetCorrect() {
+        Answer a = new Answer(false,"option",1,100,"Ana");
+        assertFalse(a.isCorrect());
+        a.setCorrect(true);
+        assertTrue(a.isCorrect());
+    }
+
+    @Test
+    void testSetOption() {
+        Answer a = new Answer(false,"option",1,100,"Ana");
+        assertEquals(a.getOption(), "option");
+        a.setOption("o");
+        assertNotEquals(a.getOption(), "option");
+        assertEquals(a.getOption(), "o");
+    }
+
+    @Test
+    void testSetGameId() {
+        Answer a = new Answer(false,"option",1,100,"Ana");
+        assertEquals(a.getGameID(), 1);
+        a.setGameID(2);
+        assertNotEquals(a.getGameID(), 1);
+        assertEquals(a.getGameID(), 2);
+    }
+
+    @Test
+    void testSetScore() {
+        Answer a = new Answer(false,"option",1,100,"Ana");
+        assertEquals(a.getScore(), 100);
+        a.setScore(2);
+        assertNotEquals(a.getScore(), 100);
+        assertEquals(a.getScore(), 2);
+    }
+
+    @Test
+    void testSetUsername() {
+        Answer a = new Answer(false,"option",1,100,"Ana");
+        assertEquals(a.getUsername(), "Ana");
+        a.setUsername("o");
+        assertNotEquals(a.getUsername(), "Ana");
+        assertEquals(a.getUsername(), "o");
     }
 }
